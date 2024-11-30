2c79f0a5a0216bb8e81964b5b6ff014b725af887
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-30 19:30:34 +0330

    edit App: add Routes

--- crypto/src/App.jsx
+++ crypto/src/App.jsx
@@ -1,8 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import NewsPage from './pages/NewsPage';
+import CryptoListPage from './pages/CryptoListPage';
+import CryptoPage from './pages/CryptoPage';
+import NotFoundPage from './pages/NotFoundPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/crypto-list" element={<CryptoListPage />} />
+          <Route path="/crypto" element={<CryptoPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
