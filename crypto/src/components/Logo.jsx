fdfe8d78d90ba5634008fbf1989a5f8540d2e929
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-30 19:29:39 +0330

    edit Logo: Add Exchange App

---
+++ crypto/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+    return (
+        <>
+            <div className="font-bold text-xl text-yellow-300">
+                <h1>Exchange App</h1>
+            </div>
+        </>
+    );
+}
+
+export default Logo;
