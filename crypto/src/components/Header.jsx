6aaae94889080ece0af7a4871d2c24d0b520b2f4
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-01 20:03:18 +0330

    edit Header: add userIcon for link to RegisterPage

--- crypto/src/components/Header.jsx
+++ crypto/src/components/Header.jsx
@@ -2,22 +2,21 @@ import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import Logo from "./Logo";
 import LoginRegisterBtn from "./LoginRegisterBtn";
+import BasicSelect from "../components/USDSelect";
+
 
 const Header = () => {
     return (
         <>
             <div className="w-full bg-red-600">
-                <div className="mx-10 p-2 flex justify-between">
-                    <div className="w-1/2 flex justify-between">
-                        <div className="w-1/3">
-                            <Logo />
-                        </div>
-                        <div className="w-2/3">
-                            <NavBar />
-                        </div>
+                <div className="mx-10 p-2 flex justify-between items-center">
+                    <div className="w-1/2 flex justify-between items-center">
+                        <div className="w-1/3"><Logo /></div>
+                        <div className="w-2/3"><NavBar /></div>
                     </div>
-                    <div className="w-1/6">
-                        <LoginRegisterBtn />
+                    <div className="w-2/12 flex justify-center items-center">
+                        <div className="w-full"><LoginRegisterBtn /></div>
+                        <div className="w-1/2"><BasicSelect /></div>
                     </div>
                 </div>
 
