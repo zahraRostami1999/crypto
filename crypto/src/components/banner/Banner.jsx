5dc93c3f37c079f2c1c58e928a24b616210d9118
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-03 21:12:38 +0330

    edit Banner: change title color to gold

--- crypto/src/components/banner/Banner.jsx
+++ crypto/src/components/banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div className={styles.container}>
             <div className="flex justify-center items-start pt-10 h-2/3 ">
-                <h1 className="text-5xl font-bold">GOLDEX</h1>
+                <h1 className="text-5xl font-bold text-yellow-400">GOLDEX</h1>
             </div>
             <div className='h-2/3'>
                 <BannerCarousel />
