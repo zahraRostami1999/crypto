ccc9f458112e430bd396c5dffbb69af91be4eba0
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-03 21:11:31 +0330

    edit Frequently Questions: add important questions

---
+++ crypto/src/components/FrequentlyQuestions.jsx
@@ -0,0 +1,33 @@
+const FrequentlyQuestions = () => {
+    return ( 
+        <>
+        <div className="mx-3 border rounded-lg px-5 py-3 mt-6">
+            <div className="mb-9">
+                <h1 className="text-3xl font-semibold text-center">Frequently Asked Questions</h1>
+            </div>
+            <div>
+                <ul>
+                    <li className="flex justify-between my-5">
+                        <p>What is the difference between Bitcoin and Ethereum?</p>
+                        <p>+</p>
+                    </li>
+                    <li className="flex justify-between my-5">
+                        <p>What is the difference between Bitcoin and Ethereum?</p>
+                        <p>+</p>
+                    </li>
+                    <li className="flex justify-between my-5">
+                        <p>What is the difference between Bitcoin and Ethereum?</p>
+                        <p>+</p>
+                    </li>
+                    <li className="flex justify-between my-5">
+                        <p>What is the difference between Bitcoin and Ethereum?</p>
+                        <p>+</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </>
+     );
+}
+ 
+export default FrequentlyQuestions;
