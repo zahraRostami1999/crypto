7e8e8ade306e422aad7a9b1726ba8abfe45b7abe
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-03 18:13:53 +0330

    edit HomePage: import Banner

--- crypto/src/pages/HomePage.jsx
+++ crypto/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
+import banner from "../assets/banner2.jpg";
+import Banner from "../components/banner/Banner";
 
 const HomePage = () => {
     return (
         <>
-            <div className="bg-red-100">HomePage</div>
-            
-
+            <div className="h-full">HomePage
+                <div className="h-screen">
+                    <Banner />
+                </div>
+            </div>
         </>
     );
 }
