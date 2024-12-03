665131060310f930e91f10fe9c3ac72ec1f9fa2a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-03 21:14:58 +0330

    edit HomePage: add CountOfMembers and FrequentQuestions

--- crypto/src/pages/HomePage.jsx
+++ crypto/src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
-import banner from "../assets/banner2.jpg";
 import Banner from "../components/banner/Banner";
+import CountOfMembers from "../components/CountOfMembers";
+import FrequentlyQuestions from "../components/FrequentlyQuestions";
 
 const HomePage = () => {
     return (
         <>
-            <div className="h-full">
-                <div className="h-screen">
+            <div className="h-full bg-zinc-950">
+                <div>
                     <Banner />
+                </div>
+                <div className="w-full flex justify-between px-10">
+                    <div className="w-2/5"><CountOfMembers /></div>
+                    <div className="w-3/5"><FrequentlyQuestions /></div>
                 </div>
             </div>
         </>
